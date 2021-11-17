3f12b03c4201196742054260f098ee3e5a6bfb2e
nei <nei.thomass@gmail.com>
2021-11-17 08:05:38 -0300

    retoma desenvolvimento do projeto
    - organiza arquivos
    - attualiza bibliotecas

--- frontend/authentication-app-vue/src/components/auth-components/home/HomeComponent.vue
+++ frontend/authentication-app-vue/src/components/auth-components/home/HomeComponent.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="row justify-content-center">
+    <div class="container text-center">
         <h1>Home Page</h1>
+        <p>Welcome to the login with JWT-NODE-VUE-MONGODB sistem...</p>
+        <p>Successfull into access this master page... <strong>explore and have fun!!!</strong></p>
     </div>
 </template>
 
