144589d7b15f1a65862fb2e5aac0d0103e7a6a13
nei <nei.thomass@gmail.com>
2021-12-02 09:18:33 -0300

    implementa e configura nprogress

--- frontend/authentication-app-vue/src/main.js
+++ frontend/authentication-app-vue/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '..node-modules/nprogress/nprogress.css';
 
 // Vue.use(Sweetalert);
 
